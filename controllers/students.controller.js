2cf5a0e9acbe28a4f97eefcd22c73fcb544a256f
Mahmood Hassan Rameem <rameem2019@gmail.com>
2024-05-08 17:41:58 +0600

    getone and delete request added

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -1,7 +1,29 @@
-const users = require("../models/students.modles");
+let users = require("../models/students.modles");
 
 const getAllStudents = (req, res) => {
   res.status(200).send(users);
 };
 
-module.exports = { getAllStudents };
+const getSingleUser = (req, res) => {
+let {id} = req.params;
+
+let getOne = users.find((user) => user.id == id)
+
+res.status(200).send(getOne)
+
+}
+
+
+const deleteOne = (req, res) => {
+  let {id} = req.params;
+
+  let newArray = users.filter((user) => user.id !== id)
+
+  users = newArray
+
+  res.status(200).send(users)
+
+}
+
+
+module.exports = { getAllStudents, getSingleUser , deleteOne};
--- routes/students.route.js
+++ routes/students.route.js
@@ -1,6 +1,8 @@
 const router = require("express").Router(); // init router
-const { getAllStudents } = require("../controllers/students.controller"); // import all controllers
+const { getAllStudents, getSingleUser, deleteOne } = require("../controllers/students.controller"); // import all controllers
 
 router.get("/", getAllStudents);
+router.get("/:id", getSingleUser);
+router.delete("/:id", deleteOne)
 
 module.exports = router;
