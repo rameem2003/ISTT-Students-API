5384fc2c8c57a0e1ab32868b8d5a7d682aa6bad3
rameem2003 <rameem2019@gmail.com>
2024-05-09 09:33:12 +0600

    update html

---
+++ views/main.js
@@ -0,0 +1,22 @@
+/**
+ * Dummy REST API
+ * Institute of Science Trade & Technology
+ * API Version 1.0
+ */
+
+[
+  {
+    _id: "66390020428c003f7a865889",
+    id: "f082f225-affd-49ec-9ad1-9bdcd9043553",
+    name: "Mahmood Hassan Rameem",
+    createdAt: "2024-05-06T16:06:56.397Z",
+    __v: 0,
+  },
+  {
+    _id: "66390061428c003f7a86588c",
+    id: "2ee51afd-d513-4e4e-8e13-73ab7d56ed28",
+    name: "Fahmida Yeasmin",
+    createdAt: "2024-05-06T16:08:01.075Z",
+    __v: 0,
+  },
+];
